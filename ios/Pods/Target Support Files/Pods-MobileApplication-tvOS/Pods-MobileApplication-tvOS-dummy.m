7d7f728f258eb6202d43d4f201274b3f26ae0681
Leuteris Mitilineos <std145287@ac.eap.gr>
2020-11-12 00:59:27 +0200

    Commit 2nd

---
+++ ios/Pods/Target Support Files/Pods-MobileApplication-tvOS/Pods-MobileApplication-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MobileApplication_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_MobileApplication_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-MobileApplication/Pods-MobileApplication-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MobileApplication : NSObject
+@end
+@implementation PodsDummy_Pods_MobileApplication
+@end
